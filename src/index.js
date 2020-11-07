bdb9ce3ab149e647b0c546f3376bc73903498764
Shawn <codingshawn@gmail.com>
2020-11-07 14:25:47 +0800

    Refactored index.js to use tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import createLandingPage from './landingpage.js';
+import createContactPage from './contact.js';
+import createMenuPage from './menu.js'
+import createTabs from './tab.js' 
 
-let content = document.getElementById("content");
-createLandingPage(content);
+let loadPage = (() => {
+    let content = document.getElementById("content");
+    let tabs = createTabs("Home", "Menu", "Contact")
+    content.appendChild(tabs);
+
+    let displayedContent = document.createElement("div");
+    content.appendChild(displayedContent);
+    createLandingPage(displayedContent)
+
+
+    const changeTab = (pageLoadFunction) => {
+        displayedContent.textContent = "";
+        pageLoadFunction(displayedContent);
+    }
+
+    tabs.addEventListener('click', (e) => {
+        if (e.target.textContent === "Home") {
+            changeTab(createLandingPage);
+        } else if (e.target.textContent === "Menu") {
+            changeTab(createMenuPage);
+        } else if (e.target.textContent === "Contact") {
+            changeTab(createContactPage);
+        }
+    })
+})();
